8f6d728c692023b8d2b38257094e22bcf17e1241
SUN JOHN <702017096@cart.ads>
2019-04-10 13:31:28 -0700

    Php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sohn's PHP</title>
+</head>
+<body>
+<h1>John's First PHP page</h1>
+<? php
+    echo "Hellow World!";
+    ?>
+
+</body>
+</html>
